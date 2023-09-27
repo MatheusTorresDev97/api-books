c72fb1204985546a7b095650933f4c4a151619f4
Matheus <matheustorresdev@gmail.com>
2023-09-27 20:19:45 -0300

    em desenvolvimento

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+import manipuladorDeErros from "./middleware/manipuladorDeErros.js";
+import manipulador404 from "./middleware/manipulador404.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+app.use(manipulador404)
+
+app.use(manipuladorDeErros);
+
+export default app;
