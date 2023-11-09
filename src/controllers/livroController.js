b811812bbfa2364470d8dd72a95fdc05589ae27c
Matheus <matheustorresdev@gmail.com>
2023-11-09 20:38:52 -0300

    part 3

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,68 +1,78 @@
-import livro from "../models/Livro.js";
-import { autor } from "../models/Autor.js";
+import { livro } from "../models/index.js";
+import { autor } from "../models/index.js";
 
 class LivroController {
-
-  static async listarLivros (req, res, next) {
+  static async listarLivros(req, res, next) {
     try {
       const listaLivros = await livro.find({});
       res.status(200).json(listaLivros);
     } catch (erro) {
-     next(erro)
+      next(erro);
     }
-  };
+  }
 
-  static async listarLivroPorId (req, res, next) {
+  static async listarLivroPorId(req, res, next) {
     try {
       const id = req.params.id;
       const livroEncontrado = await livro.findById(id);
       res.status(200).json(livroEncontrado);
     } catch (erro) {
-      next(erro)
-    }
-  };
-
-  static async cadastrarLivro (req, res, next) {
-    const novoLivro = req.body;
-    try {
-      const autorEncontrado = await autor.findById(novoLivro.autor);
-      const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc }};
-      const livroCriado = await livro.create(livroCompleto);
-      res.status(201).json({ message: "criado com sucesso", livro: livroCriado });
-    } catch (erro) {
-     next(erro)
+      next(erro);
     }
   }
 
-  static async atualizarLivro (req, res, next) {
+  static async cadastrarLivro(req, res, next) {
+    const novoLivro = req.body;
+    try {
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = {
+        ...novoLivro,
+        autor: { ...autorEncontrado._doc },
+      };
+      const livroCriado = await livro.create(livroCompleto);
+      res
+        .status(201)
+        .json({ message: "criado com sucesso", livro: livroCriado });
+    } catch (erro) {
+      next(erro);
+    }
+  }
+
+  static async atualizarLivro(req, res, next) {
     try {
       const id = req.params.id;
       await livro.findByIdAndUpdate(id, req.body);
       res.status(200).json({ message: "livro atualizado" });
     } catch (erro) {
-      next(erro)
+      next(erro);
     }
-  };
+  }
 
-  static async excluirLivro (req, res, next) {
+  static async excluirLivro(req, res, next) {
     try {
       const id = req.params.id;
       await livro.findByIdAndDelete(id);
       res.status(200).json({ message: "livro excluído com sucesso" });
     } catch (erro) {
-     next(erro)
-    }
-  };
-
-  static async listarLivrosPorEditora (req, res, next) {
-    const editora = req.query.editora;
-    try {
-      const livrosPorEditora = await livro.find({ editora: editora });
-      res.status(200).json(livrosPorEditora);
-    } catch (erro) {
-      next(erro)
+      next(erro);
     }
   }
-};
+
+  static async listarLivrosPorFiltro(req, res, next) {
+    const { editora, titulo } = req.query;
+
+    const busca = {}
+
+    if(editora) busca.editora = editora;
+    if(titulo) busca.titulo = titulo;
+
+    try {
+      const livrosPorEditora = await livro.find(busca);
+      res.status(200).json(livrosPorEditora);
+    } catch (erro) {
+      next(erro);
+    }
+  }
+}
 
 export default LivroController;
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,14 +1,37 @@
 import mongoose from "mongoose";
 import { autorSchema } from "./Autor.js";
 
-const livroSchema = new mongoose.Schema({
-  id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: String, required: [true, "O nome do livro é obrigatório"]  },
-  editora: { type: String },
-  preco: { type: Number },
-  paginas: { type: Number },
-  autor: autorSchema
-}, { versionKey: false });
+const livroSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    titulo: {
+      type: String,
+      required: [true, "O título do livro é obrigatório"],
+    },
+    editora: { type: String, required: [true, "A editora é obrigatória"] },
+    preco: {
+      type: Number,
+      required: [true, "O preço é obrigatório"],
+      enum: {
+        values: ["25", "50", "75"],
+        message: "A valor {VALUE} não é um valor permitido.",
+      },
+    },
+    paginas: {
+      type: Number,
+      // min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      // max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+      validate: {
+        validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+      }
+    },
+    autor: autorSchema,
+  },
+  { versionKey: false }
+);
 
 const livro = mongoose.model("livros", livroSchema);
 
